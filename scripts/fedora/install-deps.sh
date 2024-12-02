3650d02305cb5fa839638e0305d48db7460179fa
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2024-12-02 11:33:19 +0200

    [8] Fix Docker for Windows, add Windows automation.

--- scripts/fedora/install-deps.sh
+++ scripts/fedora/install-deps.sh
@@ -1,9 +1,10 @@
+dnf install -y bash
 #!/bin/bash
 
 set -xe
 
 dnf update -y
-dnf install -y cmake python3 python3-pip python3-virtualenv python3-devel ninja-build gcc gcc-c++ gtest-devel pybind11-devel make git redhat-rpm-config
+dnf install -y bash cmake python3 python3-pip python3-virtualenv python3-devel ninja-build gcc gcc-c++ gtest-devel pybind11-devel make git redhat-rpm-config
 
 python3 -m venv venv
 source venv/bin/activate
@@ -13,7 +14,7 @@ mkdir -p ~/.conan2/profiles
 if [ ! -e "$HOME/.conan2/profiles/default" ]; then
     echo "[settings]" > ~/.conan2/profiles/default
     echo "os=Linux" >> ~/.conan2/profiles/default
-    echo "arch=armv8" >> ~/.conan2/profiles/default
+    echo "arch=x86_64" >> ~/.conan2/profiles/default
     echo "build_type=Release" >> ~/.conan2/profiles/default
     echo "compiler=gcc" >> ~/.conan2/profiles/default
     echo "compiler.version=14" >> ~/.conan2/profiles/default
