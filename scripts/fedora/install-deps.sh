5116439866c8d0e74649ba56e75cb80da7004e18
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2024-12-02 14:42:32 +0200

    [10] Final: auto-select Docker container arch.

--- scripts/fedora/install-deps.sh
+++ scripts/fedora/install-deps.sh
@@ -10,11 +10,21 @@ python3 -m venv venv
 source venv/bin/activate
 pip install conan
 
+arch=$(uname -m)
+if [[ "$arch" == "x86_64" ]]; then
+    conan_arch="x86_64"
+elif [[ "$arch" == "aarch64" || "$arch" == "arm64" ]]; then
+    conan_arch="armv8"
+else
+    echo "Unsupported architecture: $arch"
+    exit 1
+fi
+
 mkdir -p ~/.conan2/profiles
 if [ ! -e "$HOME/.conan2/profiles/default" ]; then
     echo "[settings]" > ~/.conan2/profiles/default
     echo "os=Linux" >> ~/.conan2/profiles/default
-    echo "arch=x86_64" >> ~/.conan2/profiles/default
+    echo "arch=$conan_arch" >> ~/.conan2/profiles/default
     echo "build_type=Release" >> ~/.conan2/profiles/default
     echo "compiler=gcc" >> ~/.conan2/profiles/default
     echo "compiler.version=14" >> ~/.conan2/profiles/default
